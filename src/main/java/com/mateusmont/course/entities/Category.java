db2f05db8dfb466cd57a4e1749b3facb29186d42
Mateusmont-1 <mateus.monteiro112@gmail.com>
2024-07-20 17:20:45 -0300

    OrderStatus enum

---
+++ src/main/java/com/mateusmont/course/entities/Category.java
@@ -0,0 +1,25 @@
+package com.mateusmont.course.entities;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Category implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	
+	public Category() {
+	}
+
+	
+	
+}
