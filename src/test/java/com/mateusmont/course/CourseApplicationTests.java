918128a9b4a1e933af15ad9c92547f7aaf50c00f
Mateusmont-1 <mateus.monteiro112@gmail.com>
2024-07-20 14:41:18 -0300

    Project created

---
+++ src/test/java/com/mateusmont/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mateusmont.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
