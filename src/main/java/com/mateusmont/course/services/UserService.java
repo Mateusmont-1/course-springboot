cd9e9e8ff8d23d60b6fb5f5c86e9a080234ce85e
Mateusmont-1 <mateus.monteiro112@gmail.com>
2024-08-01 20:02:04 -0300

    Exception handling - update

--- src/main/java/com/mateusmont/course/services/UserService.java
+++ src/main/java/com/mateusmont/course/services/UserService.java
@@ -12,6 +12,8 @@ import com.mateusmont.course.repositories.UserRepository;
 import com.mateusmont.course.services.exceptions.DatabaseException;
 import com.mateusmont.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 	
@@ -42,9 +44,14 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		}
+		catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
